319877ece0f0d4cebeecfa32f2d9db6cd406dbec
Студент <st@fungi.lan>
2020-02-04 21:19:59 +0300

    Initial commit

---
+++ Assets/TextQuest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    [Header("Elements")]
+    public Text titleText;
+    public Text textOfTheGame;
+    [Header("Config")]
+    [Tooltip("Название игры")] public string title = "Hello world";
+
+    [TextArea(minLines: 10, maxLines: 20)] public string mainText = "Take your chance play the games";
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText.text = title;
+        textOfTheGame.text = mainText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
