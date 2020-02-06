5eaa472600693dbdda16e88622bea97bea2834ac
AksanaHrytsuk <oksana@redeyes.homenetwork>
2020-02-06 17:13:39 +0300

    typo

--- Assets/TextQuest.cs
+++ Assets/TextQuest.cs
@@ -11,7 +11,7 @@ public class TextQuest : MonoBehaviour
     [Header("Config")]
     [Tooltip("Название игры")] public string title = "let's get it started";
 
-    [TextArea(minLines: 10, maxLines: 20)] public string mainText = "Would you like play the game";
+    [TextArea(minLines: 10, maxLines: 20)] public string mainText = "Would you like playing the game";
     // Start is called before the first frame update
     void Start()
     {
