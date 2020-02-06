7c6395996b8702f19ed27a61794bfdaf6eb38aef
AksanaHrytsuk <oksana@redeyes.homenetwork>
2020-02-06 16:56:26 +0300

    changed var

--- Assets/TextQuest.cs
+++ Assets/TextQuest.cs
@@ -7,19 +7,19 @@ public class TextQuest : MonoBehaviour
 {
     [Header("Elements")]
     public Text titleText;
-    public Text textOfTheGame;
+    public Text contentText;
     [Header("Config")]
     [Tooltip("Название игры")] public string title = "Hello world";
 
-    [TextArea(minLines: 10, maxLines: 20)] public string mainText = "Take your chance play the games";
+    [TextArea(minLines: 10, maxLines: 20)] public string mainText = "Take your chance and play the games";
     // Start is called before the first frame update
     void Start()
     {
         titleText.text = title;
-        textOfTheGame.text = mainText;
+        contentText.text = mainText;
     }
 
-    // Update is called once per frame
+    // Update is called once per frames
     void Update()
     {
         
