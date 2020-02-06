d84df7eb97f6ed01130386e6437b8186d4d50882
Студент <st@fungi.lan>
2020-02-06 21:31:06 +0300

    lesson 4

--- Assets/Step.cs
+++ Assets/Step.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Step : MonoBehaviour
 {
     [TextArea(minLines: 10, maxLines: 20)] public string mainText = "Take your chance play the games";
+
+
+    public Step[] nextSteps;
+
 }
+
--- Assets/TextQuest.cs
+++ Assets/TextQuest.cs
@@ -11,15 +11,23 @@ public class TextQuest : MonoBehaviour
     [Header("Config")]
     [Tooltip("Название игры")] public string title = "Hello world";
 
-    
+
     // Start is called before the first frame update
     public Step activeStep;
     void Start()
     {
         titleText.text = title;
-      //  textOfTheGame.text = mainText;
+        //  textOfTheGame.text = mainText;
         textOfTheGame.text = activeStep.mainText;
-       
+
+        //    int[] numbers = { 1, 3, 7, 8, 2 };
+        //    numbers[2] = 6;
+        //    Debug.Log(numbers);
+
+        //    string[] days = { "пн", "вт", "ср", "чт", "пт", "суб", "вс" };
+        //    Debug.Log(days[5]);
+
+        //    Debug.Log(days.Length);
     }
 
     // Update is called once per frame
@@ -28,16 +36,31 @@ public class TextQuest : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
 
-            textOfTheGame.text = "Вы попали в комнату 1";
+            activeStep = activeStep.nextSteps[0];
+            textOfTheGame.text = activeStep.mainText;
 
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
+            if (activeStep.nextSteps.Length > 1)
+            {
 
-            textOfTheGame.text = "Вы попали в комнату 2";
 
+                activeStep = activeStep.nextSteps[1];
+                textOfTheGame.text = activeStep.mainText;
+            }
         }
 
-       
+
     }
+    void chekPress(int index)
+    { 
+        if (activeStep.nextSteps.Length > index)
+        {
+        if (activeStep.nextSteps[index] != null)
+        {    
+        activeStep = activeStep.nextSteps[1];
+        textOfTheGame.text = activeStep.mainText;
+        }
+     }
 }
