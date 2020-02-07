e5b3ba9242ff8681e24b241a91deca83f794245a
AksanaHrytsuk <oksana@redeyes.homenetwork>
2020-02-07 23:24:40 +0300

    added checkPress method

--- Assets/TextQuest.cs
+++ Assets/TextQuest.cs
@@ -5,62 +5,54 @@ using UnityEngine.UI;
 
 public class TextQuest : MonoBehaviour
 {
-    [Header("Elements")]
-    public Text titleText;
-    public Text textOfTheGame;
-    [Header("Config")]
-    [Tooltip("Название игры")] public string title = "Hello world";
+  [Header("Elements")]
+  public Text titleText;
+  public Text textOfTheGame;
+  [Header("Config")]
+  [Tooltip("Название игры")] public string title = "Hello world";
 
 
-    // Start is called before the first frame update
-    public Step activeStep;
-    void Start()
+  // Start is called before the first frame update
+  public Step activeStep;
+  void Start()
+  {
+    titleText.text = title;
+    //  textOfTheGame.text = mainText;
+    textOfTheGame.text = activeStep.mainText;
+
+    //    int[] numbers = { 1, 3, 7, 8, 2 };
+    //    numbers[2] = 6;
+    //    Debug.Log(numbers);
+
+    //    string[] days = { "пн", "вт", "ср", "чт", "пт", "суб", "вс" };
+    //    Debug.Log(days[5]);
+
+    //    Debug.Log(days.Length);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Alpha1))
     {
-        titleText.text = title;
-        //  textOfTheGame.text = mainText;
-        textOfTheGame.text = activeStep.mainText;
-
-        //    int[] numbers = { 1, 3, 7, 8, 2 };
-        //    numbers[2] = 6;
-        //    Debug.Log(numbers);
-
-        //    string[] days = { "пн", "вт", "ср", "чт", "пт", "суб", "вс" };
-        //    Debug.Log(days[5]);
-
-        //    Debug.Log(days.Length);
+        checkPress(1);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+        checkPress(2);
     }
 
-    // Update is called once per frame
-    void Update()
+
+  }
+  void checkPress(int index)
+  {
+    if (activeStep.nextSteps.Length > index)
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-
-            activeStep = activeStep.nextSteps[0];
-            textOfTheGame.text = activeStep.mainText;
-
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            if (activeStep.nextSteps.Length > 1)
-            {
-
-
-                activeStep = activeStep.nextSteps[1];
-                textOfTheGame.text = activeStep.mainText;
-            }
-        }
-
-
-    }
-    void chekPress(int index)
-    { 
-        if (activeStep.nextSteps.Length > index)
-        {
-        if (activeStep.nextSteps[index] != null)
-        {    
-        activeStep = activeStep.nextSteps[1];
+      if (activeStep.nextSteps[index] != null)
+      {
+        activeStep = activeStep.nextSteps[index];
         textOfTheGame.text = activeStep.mainText;
-        }
-     }
-}
+      }
+    }
+  }
+} 
